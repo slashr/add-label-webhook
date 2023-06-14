8d87ee7265f63b53fb1272a18031498c357b73f1
akash.nair <akash.nair@understand.ai>
2023-06-14 10:38:07 +0200

    Improve variable names

--- main.py
+++ main.py
@@ -13,21 +13,21 @@ uvicorn_logger.removeHandler(uvicorn_logger.handlers[0])  # Turn off uvicorn dup
 webhook.setLevel(logging.INFO)
 logging.basicConfig(format="[%(asctime)s] %(levelname)s: %(message)s")
 
-pool = environ.get("NODE_POOL")
-if not pool:
+custom_node_selector = environ.get("NODE_POOL")
+if not custom_node_selector:
     webhook.error("The required environment variable 'NODE_POOL' isn't set. Exiting...")
     exit(1)
 
 
-def patch(node_pool: str, existing_selector: bool) -> base64:
-    label, value = node_pool.replace(" ", "").split(":")
-    webhook.info(f"Got '{node_pool}' as nodeSelector label, patching...")
+def patch(custom_selector: str, existing_selector: bool) -> base64:
+    custom_selector_key, custom_selector_value = custom_selector.replace(" ", "").split(":")
+    webhook.info(f"Got '{custom_selector}' as nodeSelector, patching...")
 
     if existing_selector:
         webhook.info(f"Found already existing node selector, replacing it.")
-        patch_operations = [Patch(op="replace", value={f"{label}": f"{value}"}).dict()]
+        patch_operations = [Patch(op="replace", value={f"{custom_selector_key}": f"{custom_selector_value}"}).dict()]
     else:
-        patch_operations = [Patch(op="add", value={f"{label}": f"{value}"}).dict()]
+        patch_operations = [Patch(op="add", value={f"{custom_selector_key}": f"{custom_selector_value}"}).dict()]
     return base64.b64encode(json.dumps(patch_operations).encode())
 
 
@@ -40,7 +40,7 @@ def admission_review(uid: str, message: str, existing_selector: bool) -> dict:
             "allowed": True,
             "patchType": "JSONPatch",
             "status": {"message": message},
-            "patch": patch(pool, existing_selector).decode(),
+            "patch": patch(custom_node_selector, existing_selector).decode(),
         },
     }
 
